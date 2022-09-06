4f5af33b5847b726144cd1f7f608b1cc2f5adc8b
lyy <924514810@qq.com>
2022-09-06 10:05:44 +0800

    third commit

--- maven-git/src/main/java/com/zoomlion/GitTest.java
+++ maven-git/src/main/java/com/zoomlion/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
         System.out.println("hello java!");
+        System.out.println("hello everyone!");
     }
 }
